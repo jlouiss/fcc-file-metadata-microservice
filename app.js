806c270d41af3520e9ce4321e0847d13db3a1208
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-08-23 22:57:09 +0200

    refactored code

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'))
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,23 +1,5 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var cors = require('cors');
+const app = require('./app')
 
-// require and use "multer"...
-
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
-});
+app.listen(process.env.PORT || 3000, () => console.log(`Node.js listening on port ${process.env.PORT || 3000}`))
