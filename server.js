660ea21d834b1c51677388b95ad953fc5a158505
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-08-24 22:00:47 +0200

    using dotenv

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict'
 
+require('dotenv').config()
+
 const app = require('./app')
 
 app.listen(process.env.PORT || 3000, () => console.log(`Node.js listening on port ${process.env.PORT || 3000}`))
